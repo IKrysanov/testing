31c632a24a59c5b84c47b946405dbc375bc31f1e
IKrysanov <iljaapp0403@gmail.com>
2024-11-02 00:42:03 +0300

    1

--- test_main.py
+++ test_main.py
@@ -5,3 +5,38 @@ import allure
 def test_main():
     with allure.step("Один всегда равно одному"):
         assert 1 == 1
+
+@allure.title("Позитивный тест расчёта")
+def test_main1():
+    with allure.step("Один всегда равно одному"):
+        assert 1 == 1
+
+@allure.title("Позитивный тест расчёта")
+def test_main2():
+    with allure.step("Один всегда равно одному"):
+        assert 1 == 1
+
+@allure.title("Позитивный тест расчёта")
+def test_main3():
+    with allure.step("Один всегда равно одному"):
+        assert 1 == 1
+
+@allure.title("Позитивный тест расчёта")
+def test_main4():
+    with allure.step("Один всегда равно одному"):
+        assert 1 == 1
+
+@allure.title("Позитивный тест расчёта")
+def test_main5():
+    with allure.step("Один всегда равно одному"):
+        assert 1 == 1
+
+@allure.title("Позитивный тест расчёта")
+def test_main6():
+    with allure.step("Один всегда равно одному"):
+        assert 1 == 1
+
+@allure.title("Позитивный тест расчёта")
+def test_main7():
+    with allure.step("Один всегда равно одному"):
+        assert 1 == 1
