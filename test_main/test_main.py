0da258d7e1160fb6924b7d747444c6899285db75
IKrysanov <iljaapp0403@gmail.com>
2024-11-02 10:03:10 +0300

    1

--- test_main/test_main.py
+++ test_main/test_main.py
@@ -1,42 +1,10 @@
 import pytest
-import allure 
+import allure
+import requests
 
 @allure.title("Позитивный тест расчёта")
 def test_main():
+    response = requests.get("https://www.google.com")
     with allure.step("Один всегда равно одному"):
         assert 1 == 1
 
-@allure.title("Позитивный тест расчёта")
-def test_main1():
-    with allure.step("Один всегда равно одному"):
-        assert 1 == 1
-
-@allure.title("Позитивный тест расчёта")
-def test_main2():
-    with allure.step("Один всегда равно одному"):
-        assert 1 == 2
-
-@allure.title("Позитивный тест расчёта")
-def test_main3():
-    with allure.step("Один всегда равно одному"):
-        assert 1 == 0
-
-@allure.title("Позитивный тест расчёта")
-def test_main4():
-    with allure.step("Один всегда равно одному"):
-        assert 1 == 1
-
-@allure.title("Позитивный тест расчёта")
-def test_main5():
-    with allure.step("Один всегда равно одному"):
-        assert 1 == 1
-
-@allure.title("Позитивный тест расчёта")
-def test_main6():
-    with allure.step("Один всегда равно одному"):
-        assert 1 == 1
-
-@allure.title("Позитивный тест расчёта")
-def test_main7():
-    with allure.step("Один всегда равно одному"):
-        assert 1 == 1
